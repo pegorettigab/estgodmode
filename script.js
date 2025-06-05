0a943d23e9a230073d80bfcf6f5946803f22c749
pegorettigab <gabriel.pegoretti96@gmail.com>
2025-06-05 09:50:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+import OpenAI from "https://cdn.skypack.dev/openai";
+
+const keyInput = document.getElementById("key");
+const botaoEnviar = document.getElementById("enviar");
+const resultado = document.getElementById("resultado");
+
+botaoEnviar.addEventListener('click', async () => enviar())
+
+async function enviar() {
+  const textarea = document.getElementById("entrada");
+  const textoOriginal = textarea.value;
+
+  const promptFinal = textoOriginal.replace(/\n/g, " ") + " Quero somente a letra das respostas";
+
+  const openai = new OpenAI({
+  apiKey: keyInput.value,
+  dangerouslyAllowBrowser: true, // ⚠️ Necessário para funcionar no navegador
+  });
+
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      { role: "user", content: promptFinal }
+    ]
+  });
+
+  resultado.innerText = completion.choices[0].message.content;
+
+}
